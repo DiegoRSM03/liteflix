09e64bc634cd8ddc128a68c6975303b23aeef308
Diego Rodrigo Sanchez Moreno <diegorsm03@gmail.com>
2022-08-05 22:27:15 -0300

    Initial configuration

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { liteflixTheme } from "liteflixTheme"
+
+import { MenuContextProvider } from "context"
+import { NavBar } from "components"
+import { Upload } from "pages"
+
+function App() {
+  return (
+    <ThemeProvider theme={liteflixTheme}>
+      <BrowserRouter>
+        <MenuContextProvider>
+          <NavBar />
+        </MenuContextProvider>
+
+        <Routes>
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/" element={<>Home Page</>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
+
+export default App
