faf406e0461dba04ffe0846162530e11523d6fb5
Diego Rodrigo Sanchez Moreno <diegorsm03@gmail.com>
2022-08-09 22:20:36 -0300

    Page's title added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import { liteflixTheme } from "liteflixTheme"
 import { MenuContextProvider, UploadScreensContextProvider } from "context"
 import { Menu, NavBar } from "components"
 import { HomePage } from "pages"
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    document.title = "LiteFlix"
+  })
+
   return (
     <ThemeProvider theme={liteflixTheme}>
       <UploadScreensContextProvider>
