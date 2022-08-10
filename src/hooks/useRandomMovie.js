3f62304a0835e4aaf42263489be788674fdf9c86
Diego Rodrigo Sanchez Moreno <diegorsm03@gmail.com>
2022-08-09 22:10:41 -0300

    Show every 8 seconds a new random movie

--- src/hooks/useRandomMovie.js
+++ src/hooks/useRandomMovie.js
@@ -8,35 +8,60 @@ export const useRandomMovie = () => {
   const [currentRandomMovie, setCurrentRandomMovie] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   const [bunchOfRandomMovies, setBunchOfRandomMovies] = useState([])
+  const [alreadyShownMovies, setAlreadyShownMovies] = useState([])
+
+  const changeMovieBackground = (url) => {
+    const movieBackground = document.querySelector("#movie-background")
+    const backgroundUrl =
+      "https://image.tmdb.org/t/p/" +
+      (window.innerWidth <= 800 ? "w500" : "original") +
+      "/" +
+      url
+
+    movieBackground.setAttribute("src", backgroundUrl)
+  }
 
   const getBunchOfRandomMovies = async () => {
     setIsLoading(true)
 
     const randomMovie = "/movie/now_playing"
     const url = `https://api.themoviedb.org/3${randomMovie}?api_key=6f26fd536dd6192ec8a57e94141f8b20`
-    const response = await axios.get(url)
 
+    const response = await axios.get(url)
     const movies = response.data.results
 
     setBunchOfRandomMovies(movies)
+
+    changeMovieBackground(movies[0].backdrop_path)
     setCurrentRandomMovie(movies[0])
-
-    const movieBackground = document.querySelector("#movie-background")
-    const backgroundUrl =
-      "https://image.tmdb.org/t/p/" +
-      (window.innerWidth <= 800 ? "w500" : "original") +
-      "/" +
-      movies[0].backdrop_path
-
-    movieBackground.style.backgroundImage = `url(${backgroundUrl})`
-    movieBackground.setAttribute("src", backgroundUrl)
+    setAlreadyShownMovies([movies[0].id])
 
     setIsLoading(false)
+  }
+
+  const pickRandomMovie = () => {
+    const movieNotShown = bunchOfRandomMovies.find(
+      (movie) => !alreadyShownMovies.includes(movie.id)
+    )
+    const newAlreadyShownMovies = alreadyShownMovies
+
+    console.log({ movieNotShown, alreadyShownMovies })
+    changeMovieBackground(movieNotShown.backdrop_path)
+    setCurrentRandomMovie(movieNotShown)
+    setAlreadyShownMovies(newAlreadyShownMovies.push(movieNotShown.id))
+
+    if (alreadyShownMovies.length === bunchOfRandomMovies.length)
+      setAlreadyShownMovies([])
   }
 
   useEffect(() => {
     getBunchOfRandomMovies()
   }, [])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => pickRandomMovie(), 8000)
+    return () => clearInterval(intervalId)
+  }, [bunchOfRandomMovies])
 
   return {
     currentRandomMovie,
